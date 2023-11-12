bb8f20ba0efcb7636e7d95780bd55eb849903700
AntoinTheReis <antoniobalcaoreis@gmail.com>
2023-11-11 19:08:12 -0500

    edited sword script

--- Turnip Boy Commits Insider Trading/Assets/Scripts/sword.cs
+++ Turnip Boy Commits Insider Trading/Assets/Scripts/sword.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class sword : MonoBehaviour
 {
+
+    swordManager SwordManager;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SwordManager = GetComponentInParent<swordManager>();
+        StartCoroutine(SelfDestruct(SwordManager.shootingWaitingTime));
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator SelfDestruct(float time)
+    {
+        yield return new WaitForSecondsRealtime(time);
+        Destroy(gameObject);
     }
 }
