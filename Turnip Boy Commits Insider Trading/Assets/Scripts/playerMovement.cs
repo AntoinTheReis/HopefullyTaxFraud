9c5f7c947578b74ab5fe675e077bec89f847a501
shreeshayelameli <shreeshayelameli@gmail.com>
2023-11-13 14:36:42 -0500

    FINALLY FIXED IT (Now functional)
    99% there, might need a couple of tweaks for perfect replication of movement

--- Turnip Boy Commits Insider Trading/Assets/Scripts/playerMovement.cs
+++ Turnip Boy Commits Insider Trading/Assets/Scripts/playerMovement.cs
@@ -8,12 +8,11 @@ public class playerMovement : MonoBehaviour
     // Misc. variable declarations
     private int hozDirection;
     private int vertDirection;
-    private float velocity;
-    private bool isMoving;
-    private bool isHozMoving;
-    private bool isVertMoving;
     private int prevHozDirection;
     private int prevVertDirection;
+    private bool isMoving;
+    private float velocity;
+    private bool[] boolArray = {false, false, false, false}; // Left, Right, Up, Down
     [Header("True public")]
     public float acceleration;
     public float friction;
@@ -22,102 +21,96 @@ public class playerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Registering player inputs
+        // Registering player input
         if (Input.GetKey("left"))
         {
             hozDirection = -1;
             prevHozDirection = -1;
-            isMoving = true;
-            isHozMoving = true;
+            miscInputRegistration(0);
         }
         else if (Input.GetKey("right"))
         {
             hozDirection = 1;
             prevHozDirection = 1;
-            isMoving = true;
-            isHozMoving = true;
+            miscInputRegistration(1);
         }
         if (Input.GetKey("up"))
         {
             vertDirection = 1;
             prevVertDirection = 1;
-            isMoving = true;
-            isVertMoving = true;
+            miscInputRegistration(2);
         }
         else if (Input.GetKey("down"))
         {
-            vertDirection= -1;
+            vertDirection = -1;
             prevVertDirection = -1;
-            isMoving = true;
-            isVertMoving = true;
+            miscInputRegistration(3);
         }
 
-        // Increase acceleration unless velocity == maxVelocity
+        // Moving the player
         if (isMoving)
         {
-            velocity += acceleration;
-            
-            if (velocity > maxVelocity)
+            // Resetting previous direction variables based on boolean array
+            if (!boolArray[0] && !boolArray[1])
             {
-                velocity = maxVelocity;
+                prevHozDirection = 0;
             }
+            if (!boolArray[2] && !boolArray[3])
+            {
+                prevVertDirection = 0;
+            }
+
+            transform.position += new Vector3(hozDirection * velocity * Time.deltaTime, vertDirection * velocity * Time.deltaTime, 0);
         }
-        // Slow player to a stop
+        // Slowing the player down to a stop
         else if (!isMoving && velocity > 0)
         {
             velocity -= friction;
 
-            if (velocity < 0) 
-            { 
+            if (velocity < 0)
+            {
                 velocity = 0;
             }
-        }
 
-        // Moving the player
-        if (velocity > 0)
-        {
-            /*
-            if (isHozMoving)
-            {
-                transform.position += new Vector3(hozDirection * velocity * Time.deltaTime, 0, 0);
-            }
-            else
-            {
-                transform.position += new Vector3(prevHozDirection * velocity * Time.deltaTime, 0, 0);
-            }
-            if (isVertMoving)
-            {
-                transform.position += new Vector3(0, vertDirection * velocity * Time.deltaTime, 0);
-            }
-            else
-            {
-                transform.position += new Vector3(0, prevVertDirection * velocity * Time.deltaTime, 0);
-            }
-            */
-
-            if (isMoving)
-            {
-                transform.position += new Vector3(hozDirection * velocity * Time.deltaTime, vertDirection * velocity * Time.deltaTime, 0);
-            }
-            else
-            {
-                transform.position += new Vector3(prevHozDirection * velocity * Time.deltaTime, prevVertDirection * velocity * Time.deltaTime, 0);
-            }
+            transform.position += new Vector3(prevHozDirection * velocity * Time.deltaTime, prevVertDirection * velocity * Time.deltaTime, 0);
         }
         resetVars();
     }
 
-    // Resetting variables for the Update loop
+    // Accelerating
+    private void accelerating()
+    {
+        velocity += acceleration;
+
+        if (velocity > maxVelocity) 
+        { 
+            velocity = maxVelocity;
+        }
+    }
+
+    // Method to consolidate redundancy in the input registration
+    private void miscInputRegistration(int index)
+    {
+        isMoving = true;
+        boolArray[index] = true;
+        accelerating();
+    }
+
+    // Resetting variables in the Update loop
     private void resetVars()
     {
         isMoving = false;
-        isHozMoving = false;
-        isVertMoving = false;
+        for (int i = 0; i < 4;  i++)
+        {
+            boolArray[i] = false;
+        }
+        vertDirection = 0;
+        hozDirection = 0;
     }
 
     // For print debugging
     void LateUpdate()
     {
-        
+        print(velocity);
     }
 }
