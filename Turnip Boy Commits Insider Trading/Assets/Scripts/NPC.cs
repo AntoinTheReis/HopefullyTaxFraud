5991e79215dbf8b7f0a638966cfbd1f988a02a0d
shreeshayelameli <shreeshayelameli@gmail.com>
2023-11-19 01:16:26 -0500

    Revert "Almost implemented"
    This reverts commit 12fac665a9469eb9f1665b8df4d5485ca21586ba.

--- Turnip Boy Commits Insider Trading/Assets/Scripts/NPC.cs
+++ Turnip Boy Commits Insider Trading/Assets/Scripts/NPC.cs
@@ -1,51 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class NPC : MonoBehaviour
 {
 
     private bool playerClose;
-    private bool dialogueOn;
-    private bool interactHit;
     [Header("True public")]
-    public GameObject Canvas;
-    public GameObject Radish_talker;
-    public GameObject NPC_talker;
-    public GameObject Background;
-    public GameObject dialogueBox;
-    public GameObject interactSymbol;
-    public GameObject nameBox;
-    public GameObject npcDialogue;
-    public GameObject npcName;
-    public string myDialogue;
-    public string myName;
+    public string dialogue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
     // Update is called once per frame
     void Update()
     {
-        // When player interacts with self
-        if (playerClose && Input.GetKeyDown(KeyCode.Z) && !dialogueOn)
+        if (playerClose && Input.GetKeyDown(KeyCode.Z))
         {
-            dialogueUI_switcher(true);
-            npcDialogue.GetComponent<Text>().text = myDialogue;
-            npcName.GetComponent<Text>().text = myName;
-            dialogueOn = true;
-            interactHit = true;
+            
         }
-
-        // When player is actively talking to self (bro, this mf won't stop saying shit like "???" Like dawg, say something!)
-        if (dialogueOn && Input.GetKeyDown(KeyCode.Z) && !interactHit)
-        {
-            dialogueUI_switcher(false);
-            dialogueOn = false;
-        }
-
-        interactHit = false;
     }
 
-    // When player gets close to self
+    // When player gets close to this
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.tag == "Player")
@@ -62,16 +41,4 @@ public class NPC : MonoBehaviour
             playerClose = false;
         }
     }
-
-    // Activate or deactivate NPC dialogue UI
-    private void dialogueUI_switcher(bool boolean)
-    {
-        Canvas.SetActive(boolean);
-        Radish_talker.SetActive(boolean);
-        NPC_talker.SetActive(boolean);
-        Background.SetActive(boolean);
-        dialogueBox.SetActive(boolean);
-        interactSymbol.SetActive(boolean);
-        nameBox.SetActive(boolean);
-    } 
 }
