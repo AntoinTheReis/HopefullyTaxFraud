c319a54dc1dad791fbb30adea336eba16f190a8d
shreeshayelameli <shreeshayelameli@gmail.com>
2023-12-08 03:03:53 -0500

    Lots of progress on Health UI
    Good stuff, couple of problems, sleepy, go to bedington now

---
+++ Turnip Boy Commits Insider Trading/Assets/Scripts/Health_UI.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health_UI : MonoBehaviour
+{
+    [Header("Heart sprite assets")]
+    public Sprite fullHeart;
+    public Sprite halfHeart;
+    public Sprite emptyHeart;
+}
--- Turnip Boy Commits Insider Trading/Assets/Scripts/playeVenemy.cs
+++ Turnip Boy Commits Insider Trading/Assets/Scripts/playeVenemy.cs
@@ -5,13 +5,21 @@ using UnityEngine;
 public class playeVenemy : MonoBehaviour
 {
 
-    public float hp = 3;
     private playerMovement playerMovement;
+    private GameObject Heart_1;
+    private GameObject Heart_2;
+    private GameObject Heart_3;
+    [Header("HP & Health_UI Object")]
+    public float hp = 3;
+    public GameObject Health_UI;
 
     // Start is called before the first frame update
     void Start()
     {
         playerMovement = GetComponent<playerMovement>();
+        Heart_1 = this.gameObject.transform.GetChild(0).gameObject;
+        Heart_2 = this.gameObject.transform.GetChild(1).gameObject;
+        Heart_3 = this.gameObject.transform.GetChild(2).gameObject;
     }
 
     // Update is called once per frame
@@ -21,6 +29,9 @@ public class playeVenemy : MonoBehaviour
         {
             hp = 3;
         }
+
+        // Update the health UI
+        HealthUIUpdate();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -38,8 +49,44 @@ public class playeVenemy : MonoBehaviour
         }
     }
 
+    // TODO: Dying and respawning
     private void Dead()
     {
         Debug.Log("dead");
+    }
+
+    // Series of methods for the health UI to reflect current health value
+        // Main health UI changing function
+    private void HealthUIUpdate()
+    {
+        // Reference to sprite objects
+        Health_UI sprites = Health_UI.GetComponent<Health_UI>();
+
+        // Sprite assignments
+        Heart_1.GetComponent<SpriteRenderer>().sprite = StateSetter(sprites, 1);
+        Heart_2.GetComponent<SpriteRenderer>().sprite = StateSetter(sprites, 2);
+        Heart_3.GetComponent<SpriteRenderer>().sprite = StateSetter(sprites, 3);
+
+    }
+        // Function to determine state heart is in (full, half, or empty) and assign that sprite to the corresponding heart object
+    private Sprite StateSetter(Health_UI spr, int num)
+    {
+        Sprite returnSpr = Sprite.Create(Texture2D.redTexture, new Rect(0.0f, 0.0f, 0.0f, 0.0f), new Vector2(0.0f, 0.0f));
+
+        // Determining heart state and returning it to the HealthUIUpdate function
+        if (hp >= num)
+        {
+            returnSpr = spr.fullHeart;
+        }
+        else if (hp == num - 0.5f)
+        {
+            returnSpr = spr.halfHeart;
+        }
+        else if (hp < num - 0.5f)
+        {
+            returnSpr = spr.emptyHeart;
+        }
+
+        return returnSpr;
     }
 }
